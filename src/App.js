b84cd4013002b993b7033444489ccb0af76c9a53
Tushar Panja <panja.tushar15@gmail.com>
2022-12-16 17:29:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  // Constructor 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code 
+  componentDidMount() {
+    fetch(
+      "http://localhost:8888/events")
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.data)
+        this.setState({
+          items: json.data,
+          DataisLoaded: true
+        });
+      })
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return <div>
+      <h1> Pleses wait some time.... </h1> </div>;
+
+
+
+    return (
+      <div className="App">
+        <h1> FORMULA 1 </h1>
+        <br></br>
+        <br></br>
+        {items.map(element => (
+          <div>
+            <h3>{element.eventName}</h3>
+            <p>{element.eventDate}</p>
+            <p>{element.eventLocation}</p>
+            <br></br>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
