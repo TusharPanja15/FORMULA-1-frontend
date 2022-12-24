c876a1cd89e0be21e5ee07cb028599e4bd3197f5
Tushar Panja <panja.tushar15@gmail.com>
2022-12-24 22:03:37 +0530

    ui-demo

---
+++ src/components/Events/EventData/EventTitle.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EventImage = props => {
+
+    return (
+        <div className={`${props.className}`}>
+            <h3>SPAIN</h3>
+            <h1>FORMULA 1 GRAN PREMIO DE SPAIN 2022</h1>
+            <p>28-30 Aug</p>
+        </div>
+    );
+};
+
+export default EventImage;
